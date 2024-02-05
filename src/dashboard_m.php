2ac2f81e6cbea98c4bfd264c90650d78ffbb449e
T-LEstr <thomas@softtop.co.uk>
2024-02-05 22:08:24 +0000

    Dark mode for manager dashboard

--- src/dashboard_m.php
+++ src/dashboard_m.php
@@ -1,4 +1,13 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+set_error_handler("handleError");
+function handleError($errno, $errstr)
+{
+    echo "<b>Error:</b> [$errno] $errstr";
+    die();
+}
 include "db_connection.php";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
@@ -6,7 +15,7 @@ if (!$conn) {
     die();
 }
 // Select count of number of tasks with progress 1 and count total number (including uncompleted) of tasks, due_date group by project_ID, join project on project_ID and get the project_title and due_date and first 15 characters of description if exists
-$sql = "SELECT project_ID, project_title, due_date, LEFT(description, 15) as description, COUNT(CASE WHEN progress = 1 THEN 1 END) as completed_tasks, COUNT(progress) as total_tasks FROM tasks INNER JOIN project ON tasks.project_ID = project.project_ID GROUP BY tasks.project_ID";
+$sql = "SELECT project.project_ID, project_title, project.due_date, LEFT(project.description, 15) as description, COUNT(CASE WHEN progress = 1 THEN 1 END) as completed_tasks, COUNT(progress) as total_tasks FROM project LEFT JOIN tasks ON tasks.project_ID = project.project_ID GROUP BY project.project_ID";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 0) {
     echo "No projects found.";
@@ -19,6 +28,42 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 
 ?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+
+    .card {
+        transition: box-shadow 0.3s ease;
+    }
+
+    .card:hover {
+        box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .15);
+    }
+</style>
+
+<script>
+    $(document).ready(function() {
+        <?php if ($colour == "text-light bg-dark") { ?>
+            $("*").each(function() {
+                if ($(this).hasClass("no-dark") == false) {
+                    $(this).addClass("text-light bg-dark");
+                }
+            });
+        <?php } ?>
+    })
+</script>
 
 <body>
     <div class="min-vh-100">
@@ -29,7 +74,7 @@ while ($row = mysqli_fetch_assoc($result)) {
                 </div>
                 <div class="col">
                     <div class="d-flex justify-content-end">
-                        <a href="./create_project_m.html" class="btn btn-primary">Create New Project</a>
+                        <a href="./create_project.php" class="btn btn-primary">Create New Project</a>
                     </div>
                 </div>
             </div>
@@ -43,12 +88,17 @@ while ($row = mysqli_fetch_assoc($result)) {
                                     <?php echo $project["project_title"]; ?>
                                 </h4>
                                 <!-- Convert date into number of days and display as subtitle -->
-                                <h6 class="card-subtitle mb-2 text-secondary">Due in
-                                    <?php echo (strtotime($project["due_date"]) - time()) / (60 * 60 * 24); ?> days
+                                <h6 class="card-subtitle mb-2 text-secondary no-dark">Due on
+                                    <?php echo $project["due_date"]; ?>
                                 </h6>
-                                <?php $completionPercentage = $project["completed_tasks"] / $project["total_tasks"] * 100; ?>
-                                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="<?php echo $completionPercentage; ?>" aria-valuemin="0" aria-valuemax="100">
-                                    <div class="progress-bar" style="width: <?php echo $completionPercentage; ?>%"></div>
+                                <?php if ($project["total_tasks"] > 0) {
+                                    $completionPercentage = $project["completed_tasks"] / $project["total_tasks"] * 100;
+                                } else {
+                                    $completionPercentage = 0;
+                                }
+                                ?>
+                                <div class="progress border no-dark" role="progressbar" aria-label="Basic example" aria-valuenow="<?php echo $completionPercentage; ?>" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar no-dark" style="width: <?php echo $completionPercentage; ?>%"></div>
                                 </div>
                                 <p class="card-text">
                                     <?php echo $project["description"]; ?>
@@ -62,6 +112,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         </main>
     </div>
 </body>
+
 
 
 
