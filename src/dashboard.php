db1fe64b81c38a9fa227ac17dc88c3ac4fa561cd
T-LEstr <thomas@softtop.co.uk>
2024-02-08 15:20:24 +0000

    Made dashboard more secure

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,24 +1,26 @@
-<?php 
-            session_start();
-			if(!isset($_SESSION["role"])){
-				echo "<script>window.location.href='./login.php'</script>";
-			}else if($_SESSION["role"] == "Manager"){
-				$taskcreate = "link-dark";
-				$topicview = "link-dark";
-				$dashview = "border-bottom border-primary link-primary";
-				include "./navbar_m.php";
-				include "./dashboard_m.php";
-			}else if($_SESSION["role"] == "TL"){
-				$topicview = "link-dark";
-				$taskcreate = "link-dark";
-				$taskview = "link-dark";
-				$dashview = "border-bottom border-primary link-primary";
-				include "./navbar_tl.php";
-				include "./view_team_tl.php";
-			}else if($_SESSION["role"] == "Employee"){
-				$topicview = "link-dark";
-				$dashview = "border-bottom border-primary link-primary";
-				include "./navbar_e.php";
-				include "./dashboard_e.php";
-			}
-		?>
+<?php
+	session_start();
+	if (!isset($_SESSION["role"])) {
+		echo "Test";
+		header('location: ./login.php');
+		//die();
+	} else if ($_SESSION["role"] == "Manager") {
+		$taskcreate = "link-dark";
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_m.php";
+		include "./dashboard_m.php";
+	} else if ($_SESSION["role"] == "TL") {
+		$topicview = "link-dark";
+		$taskcreate = "link-dark";
+		$taskview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_tl.php";
+		include "./view_team_tl.php";
+	} else if ($_SESSION["role"] == "Employee") {
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_e.php";
+		include "./dashboard_e.php";
+	}
+?>
