ac396ccffb977fd706ffecfbf520afbc2c8d39c9
FreddieMB <147514499+FreddieMB@users.noreply.github.com>
2024-02-11 20:57:14 +0000

    Card updates, overflow, async search

--- src/asyncposts.php
+++ src/asyncposts.php
@@ -4,56 +4,68 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   echo "Connection Error." ;
 }
+
 if (isset($_POST['asyncINT_ID'])) {
     $INT_ID = $_POST['asyncINT_ID'];
 }
 
+function setpic($img_url) {
+  if ($img_url == 'null') {
+      return "grey.png";
+  } else {
+      return $img_url;
+  }
+}
 
-if (isset($_POST['sortby']))
-{
-$PHPID = $_POST['sortby'];
+if (isset($_POST['search'])) {
+  $searchInput = $_POST['search'];
+  $Topic_search = '%' . $searchInput . '%';
+
+  $sql = "SELECT title, content, img_url, post_ID, topic_ID  FROM posts WHERE (topic_ID = $INT_ID) AND (LOWER(title) LIKE '$Topic_search') ORDER BY title ASC";
+  $Result = mysqli_query($conn, $sql);
+  
+  while($resultA = mysqli_fetch_array($Result)){
+
+    $PIC = setpic($resultA["img_url"]);
+
+    echo '
+        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
+        <div class="card mx-auto">
+        <img src="' . $PIC . '"
+          class="card_img" alt="...">
+        <div class="card-body" style="height: 68px; overflow: hidden;">
+          <p class="card-text mb-0">' . $resultA["title"] . '</p>
+          <a class="stretched-link" href="./view_ind_post_m.php?POST_ID=' . $resultA["post_ID"] . '"></a>
+          </div>
+          </div>
+    </div>';  
+  
+}
 
 }
 
 
-switch($PHPID){
+elseif (isset($_POST['sortby'])){
+  $PHPID = $_POST['sortby'];
 
-    case "Search":
-      $sql = "SELECT title, content, img_url  FROM posts WHERE LOWER(title) LIKE '$Topic_search' ORDER BY title ASC";
-      $Result = mysqli_query($conn, $sql);
-      
-      while($resultA = mysqli_fetch_array($Result)){
-  
-        echo '
-        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
-        <div class="card mx-auto">
-            <img src="https://thumbs.dreamstime.com/b/software-engineer-portrait-smiling-young-vietnamese-69422682.jpg"
-              class="card_img" alt="...">
-            <div class="card-body">
-              <p class="card-text mb-0">' . $resultA["title"] . $resultA["content"] . '</p>
-              <a class="stretched-link" href="./view_ind_post_m.php"></a>
-              </div>
-              </div>
-        </div>';  
-      }
-  
-      break;
+switch($PHPID){
   
   case "ABC":
           
-    $sql = "SELECT title, content, img_url  FROM posts WHERE topic_ID = $INT_ID ORDER BY title ASC";
+    $sql = "SELECT title, content, img_url, post_ID  FROM posts WHERE topic_ID = $INT_ID ORDER BY title ASC";
     $Result = mysqli_query($conn, $sql);
     
     while($resultA = mysqli_fetch_array($Result)){
-    
+      $PIC = setpic($resultA["img_url"]);
+
     echo '
     <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
     <div class="card mx-auto">
-        <img src="https://thumbs.dreamstime.com/b/software-engineer-portrait-smiling-young-vietnamese-69422682.jpg"
+        <img src="' . $PIC . '"
           class="card_img" alt="...">
-        <div class="card-body">
-          <p class="card-text mb-0">' . $resultA["title"] . $resultA["content"] . '</p>
-          <a class="stretched-link" href="./view_ind_post_m.php"></a>
+        <div class="card-body" style="height: 68px; overflow: hidden;">
+          <p class="card-text mb-0">' . $resultA["title"] . '</p>
+          <a class="stretched-link" href="./view_ind_post_m.php?POST_ID=' . $resultA["post_ID"] . '"></a>
           </div>
           </div>
     </div>';
@@ -65,19 +77,20 @@ switch($PHPID){
   
   case "Date":
           
-    $sql = "SELECT title, content, img_url  FROM posts WHERE topic_ID = $INT_ID ORDER BY DATE ASC";
+    $sql = "SELECT title, content, img_url, post_ID  FROM posts WHERE topic_ID = $INT_ID ORDER BY DATE ASC";
     $Result = mysqli_query($conn, $sql);
     
     while($resultA = mysqli_fetch_array($Result)){
+      $PIC = setpic($resultA["img_url"]);
     
     echo '
     <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
     <div class="card mx-auto">
-        <img src="https://thumbs.dreamstime.com/b/software-engineer-portrait-smiling-young-vietnamese-69422682.jpg"
+        <img src="' . $PIC . '"
           class="card_img" alt="...">
-        <div class="card-body">
-          <p class="card-text mb-0">' . $resultA["title"] . $resultA["content"] . '</p>
-          <a class="stretched-link" href="./view_ind_post_m.php"></a>
+        <div class="card-body" style="height: 68px; overflow: hidden;">
+          <p class="card-text mb-0">' . $resultA["title"] . '</p>
+          <a class="stretched-link" href="./view_ind_post_m.php?POST_ID=' . $resultA["post_ID"] . '"></a>
           </div>
           </div>
     </div>';
@@ -88,19 +101,21 @@ switch($PHPID){
   break;
   
   case "View":
-    $sql = "SELECT title, content, img_url  FROM posts WHERE topic_ID = $INT_ID ORDER BY views DESC";
+    $sql = "SELECT title, content, img_url, post_ID  FROM posts WHERE topic_ID = $INT_ID ORDER BY views DESC";
     $Result = mysqli_query($conn, $sql);
     
     while($resultA = mysqli_fetch_array($Result)){
+      $PIC = setpic($resultA["img_url"]);
+      
     
     echo '
     <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
     <div class="card mx-auto">
-        <img src="https://thumbs.dreamstime.com/b/software-engineer-portrait-smiling-young-vietnamese-69422682.jpg"
+        <img src="' . $PIC . '"
           class="card_img" alt="...">
-        <div class="card-body">
-          <p class="card-text mb-0">' . $resultA["title"] . $resultA["content"] . '</p>
-          <a class="stretched-link" href="./view_ind_post_m.php"></a>
+        <div class="card-body" style="height: 68px; overflow: hidden;">
+          <p class="card-text mb-0">' . $resultA["title"] . '</p>
+          <a class="stretched-link" href="./view_ind_post_m.php?POST_ID=' . $resultA["post_ID"] . '"></a>
           </div>
           </div>
     </div>';
@@ -112,19 +127,20 @@ switch($PHPID){
   
   default:
           
-  $sql = "SELECT title, content, img_url  FROM posts WHERE topic_ID = $INT_ID ORDER BY title ASC";
+  $sql = "SELECT title, content, img_url, post_ID  FROM posts WHERE topic_ID = $INT_ID ORDER BY title ASC";
   $Result = mysqli_query($conn, $sql);
   
   while($resultA = mysqli_fetch_array($Result)){
+    $PIC = setpic($resultA["img_url"]);
   
   echo '
   <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 ">
   <div class="card mx-auto">
-      <img src="https://thumbs.dreamstime.com/b/software-engineer-portrait-smiling-young-vietnamese-69422682.jpg"
+      <img src="' . $PIC . '"
         class="card_img" alt="...">
-      <div class="card-body">
-        <p class="card-text mb-0">' . $resultA["title"] . $resultA["content"] . '</p>
-        <a class="stretched-link" href="./view_ind_post_m.php"></a>
+      <div class="card-body" style="height: 68px; overflow: hidden;">
+        <p class="card-text mb-0">' . $resultA["title"] . '</p>
+        <a class="stretched-link" href="./view_ind_post_m.php?POST_ID=' . $resultA["post_ID"] . '"></a>
         </div>
         </div>
   </div>';
@@ -132,4 +148,7 @@ switch($PHPID){
   }
       
   }
+}
+
 ?>
+
