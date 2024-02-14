f3cae07741cac649033e3298272c6e2be952686c
FreddieMB <147514499+FreddieMB@users.noreply.github.com>
2024-02-14 17:47:10 +0000

    fixing

--- src/navbar_tl.php
+++ src/navbar_tl.php
@@ -82,8 +82,8 @@
 					<li><a href="./view_tasks.php" class="nav-link px-2  settingsCSS <?php if($taskview=="link-dark"){echo $colour;} echo $taskview;?>">View Tasks</a></li>
 				</ul>
 
-				<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action="./view_topics_tl.php">
-					<input type="search" class="form-control" placeholder="Search Topics" aria-label="Search">
+				<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" action="./view_topics.php" method="get">
+					<input type="search" class="form-control" placeholder="Search Topics" aria-label="Search" name="NavbarTopic">
 				</form>
 
 				<div class="dropdown text-end">
