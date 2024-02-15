65fbed0ae888fc592041652f11a4c4fdf0420a4f
FreddieMB <147514499+FreddieMB@users.noreply.github.com>
2024-02-15 00:08:45 +0000

    topics now linked with project, can only see these certain topics if in the project its linked to or are a manager

--- src/asynctopics.php
+++ src/asynctopics.php
@@ -1,7 +1,58 @@
-
-
-
 <?php 
+
+function TopicList($resultA) {
+
+    return '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
+    onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">
+    <div style="display: inline-block; width: 100%">
+        <p>' . $resultA["title"] . '</p>
+        <div style="float: right;height: 20px;position: relative;">
+            <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
+                <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
+                <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
+            </span>
+        </div>
+    </div>
+</div>
+<br>'; 
+
+}
+
+function TopicChecker($resultA, $conn) {
+    if (isset($_POST['asyncUSER_ID'])) {
+        $USERS_ID = $_POST['asyncUSER_ID'];
+    }
+    if (isset($_POST['asyncROLE_ID'])) {
+        $ROLL_ID = $_POST['asyncROLE_ID'];
+    }
+    if (($resultA['project_ID'] !== null)){
+        $prjTEST = $resultA['project_ID'];
+        $sqlProjectCheck = "SELECT project_ID FROM project";
+        $resultProjectCheck = mysqli_query($conn, $sqlProjectCheck);
+        while($ProjectID_LIST = mysqli_fetch_assoc($resultProjectCheck)){
+            if (($ProjectID_LIST['project_ID'] == $prjTEST)) {
+                if(($ROLL_ID == "Manager")){
+                    echo TopicList($resultA);
+                } 
+                else {
+                $sqlProject = "SELECT user_ID FROM tasks WHERE project_id = $prjTEST";
+                $resultProject = mysqli_query($conn, $sqlProject);
+                    while($UserID_LIST = mysqli_fetch_assoc($resultProject)){
+                        if (($UserID_LIST['user_ID'] == $USERS_ID)) {
+                             echo TopicList($resultA);
+                                break;
+                        }
+            }
+        }
+                
+            }
+        }
+        
+    } else {
+        echo TopicList($resultA);
+    }
+}
+
 include "db_connection.php";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
@@ -14,24 +65,12 @@ if (isset($_POST['search'])) {
     $searchInput = $_POST['search'];
     $Topic_search = '%' . $searchInput . '%';
 
-    $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID WHERE LOWER(topic.title) LIKE '$Topic_search' GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
+    $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID WHERE LOWER(topic.title) LIKE '$Topic_search' GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
     $Result = mysqli_query($conn, $sql);
         
         while($resultA = mysqli_fetch_array($Result)){
     
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-            onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">
-            <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';    
+            TopicChecker($resultA, $conn);    
         }
 
 }
@@ -42,117 +81,57 @@ elseif (isset($_POST['sortby'])){
 
 switch($PHPID){
     case "ABC":
-        $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
+        $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
         $Result = mysqli_query($conn, $sql);
         
         while($resultA = mysqli_fetch_array($Result)){
     
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-            onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">
-            <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';    
+            TopicChecker($resultA, $conn);    
         }
 
 
         break;
 
     case "Date":
-        $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.date ASC";
+        $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.date ASC";
         $Result = mysqli_query($conn, $sql);
         
         while($resultA = mysqli_fetch_array($Result)){
     
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-        onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">            
-        <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';    
+            TopicChecker($resultA, $conn);    
         }
 
         break;
 
     case "Post":
-        $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY COUNT(post.topic_ID) DESC";
+        $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY COUNT(post.topic_ID) DESC";
         $Result = mysqli_query($conn, $sql);
         
         while($resultA = mysqli_fetch_array($Result)){
     
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-        onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">            
-        <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';    
+            TopicChecker($resultA, $conn);     
         }
 
         break;
 
     case "View":
-        $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.views DESC";
+        $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.views DESC";
         $Result = mysqli_query($conn, $sql);
         
         while($resultA = mysqli_fetch_array($Result)){
     
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-        onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">            
-        <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';    
+            TopicChecker($resultA, $conn);     
         }
 
         break;
 
     default:
-        $sql = "SELECT topic.topic_ID, topic.title, topic.views, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
+        $sql = "SELECT topic.topic_ID, topic.title, topic.views, topic.project_ID, COUNT(post.topic_ID) FROM topics topic LEFT JOIN posts post ON topic.topic_ID = post.topic_ID GROUP BY topic.topic_ID, topic.title, topic.views ORDER BY topic.title";
         $Result = mysqli_query($conn, $sql);
     
         while($resultA = mysqli_fetch_array($Result)){
 
-        echo '<div type="button" style="top: 495px; overflow: hidden;" class="topic1 col-xl"
-        onclick="window.location.href=\'./view_posts.php?Post_topic_ID=' . $resultA["topic_ID"] . '\';">            
-        <div style="display: inline-block; width: 100%">
-                <p>' . $resultA["title"] . '</p>
-                <div style="float: right;height: 20px;position: relative;">
-                    <span style="font-size: 17px;position: absolute;right: 5px;width: 220px;bottom: 15px;">
-                        <img src="posts-icon.png" alt="" style="height: 20px; width: 20px;"> posts: ' . $resultA["COUNT(post.topic_ID)"] . '
-                        <img src="view-icon.png" alt="" style="height: 20px; width: 20px;margin-left: 15px;"> views: ' . $resultA["views"] . '
-                    </span>
-                </div>
-            </div>
-        </div>
-        <br>';
+            TopicChecker($resultA, $conn);
 
     } 
     
