d4a13b8c90fdd9913b55901b2b6c0693a3971151
Damo-DB <147632035+Damo-DB@users.noreply.github.com>
2024-02-18 04:17:38 +0000

    All major features should be impletented
    Includes team leaders managing multiple teams, managers viewing teams, team leaders leading no teams, footers have been added

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,45 +1,45 @@
 <?php
-session_start();
-if (!isset($_SESSION["role"])) {
-	header('location: ./login.php');
-	die();
-} else if ($_SESSION["role"] == "Manager") {
-	$taskcreate = "link-dark";
-	$topicview = "link-dark";
-	$dashview = "border-bottom border-primary link-primary";
-	include "./navbar_m.php";
-	include "./dashboard_m.php";
-	include "./footer.php";
-} else if ($_SESSION["role"] == "TL") {
-	$topicview = "link-dark";
-	$taskcreate = "link-dark";
-	$taskview = "link-dark";
-	$dashview = "border-bottom border-primary link-primary";
-
-	try {
-		include "db_connection.php";
-		$conn = new PDO("mysql:host=localhost;dbname=make_it_all", $username, $password);
-	} catch (PDOException $e) {
-		echo "<script>alert('Failed to connect to database');</script>";
-		exit();
+	session_start();
+	if (!isset($_SESSION["role"])) {
+		header('location: ./login.php');
+		//die();
+	} else if ($_SESSION["role"] == "Manager") {
+		$taskcreate = "link-dark";
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_m.php";
+		include "./dashboard_m.php";
+		
+	} else if ($_SESSION["role"] == "TL") {
+		$topicview = "link-dark";
+		$taskcreate = "link-dark";
+		$taskview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		
+		try {
+			include "db_connection.php";
+			$conn = new PDO("mysql:host=localhost;dbname=make_it_all", $username, $password);
+		} catch (PDOException $e) {
+			echo "<script>alert('Failed to connect to database');</script>";
+			exit();
+		}
+		$result = $conn->query("SELECT COUNT(project_ID) FROM project WHERE team_leader = ".$_SESSION["user_ID"]." GROUP BY team_leader;");
+		$numOfProjectLeads = $result->fetch(PDO::FETCH_NUM)[0];
+		
+		include "./navbar_tl.php";
+		if ($numOfProjectLeads == 0) {
+			echo "<p style='text-align:center;margin: 5rem 2rem'>You currently do not lead any teams.</p>";
+			include "./footer.php";
+		} else if ($numOfProjectLeads == 1) {
+			include "./view_team.php";
+		} else {
+			include "./view_many_teams_tl.php";
+		}
+	
+	} else if ($_SESSION["role"] == "Employee") {
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_e.php";
+		include "./dashboard_e.php";
 	}
-	$result = $conn->query("SELECT COUNT(project_ID) FROM project WHERE team_leader = " . $_SESSION["user_ID"] . " GROUP BY team_leader;");
-	$numOfProjectLeads = $result->fetch(PDO::FETCH_NUM)[0];
-
-	include "./navbar_tl.php";
-	if ($numOfProjectLeads == 0) {
-		echo "<p style='text-align:center;margin: 5rem 2rem'>You currently do not lead any teams.</p>";
-	} else if ($numOfProjectLeads == 1) {
-		include "./view_team_tl.php";
-	} else {
-		include "./view_many_teams_tl.php";
-		exit;
-	}
-	include "./footer.php";
-} else if ($_SESSION["role"] == "Employee") {
-	$topicview = "link-dark";
-	$dashview = "border-bottom border-primary link-primary";
-	include "./navbar_e.php";
-	include "./dashboard_e.php";
-	include "./footer.php";
-}
+?>
--- src/view_many_teams_tl.php
+++ src/view_many_teams_tl.php
@@ -15,8 +15,12 @@ if (!$conn) {
     die();
 }
 
-// Select count of number of tasks with progress 1 and count total number (including uncompleted) of tasks, due_date group by project_ID, join project on project_ID and get the project_title and due_date and first 15 characters of description if exists
-$sql = "SELECT project.project_ID, project_title, project.due_date, LEFT(project.description, 15) as description, COUNT(CASE WHEN progress = 1 THEN 1 END) as completed_tasks, COUNT(progress) as total_tasks FROM project LEFT JOIN tasks ON tasks.project_ID = project.project_ID WHERE project.team_leader = ".$_SESSION["user_ID"]." GROUP BY project.project_ID ORDER BY project.due_date ASC;";
+// Select all projects and count the total number of tasks vs completed tasks. 
+// Get around one line of description to avoid overflow.
+$sql = "SELECT project.project_ID, project_title, project.due_date, LEFT(project.description, 15) as description,
+		COUNT(CASE WHEN progress = 1 THEN 1 END) as completed_tasks, COUNT(progress) as total_tasks
+		FROM project LEFT JOIN tasks ON tasks.project_ID = project.project_ID
+		WHERE project.team_leader = ".$_SESSION["user_ID"]." GROUP BY project.project_ID ORDER BY project.due_date ASC;";
 $result = mysqli_execute_query($conn, $sql);
 if (mysqli_num_rows($result) == 0) {
     echo "No projects found.";
@@ -26,8 +30,6 @@ $array = array();
 while ($row = mysqli_fetch_assoc($result)) {
     $array[] = $row;
 }
-
-
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <style>
@@ -58,7 +60,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     $(document).ready(function() {
         <?php if ($colour == "text-light bg-dark") { ?>
             $("*").each(function() {
-                if ($(this).hasClass("no-dark") == false && $(this).parents("header").length == 0){
+                if ($(this).hasClass("no-dark") == false && $(this).parents("header").length == 0) {
                     $(this).addClass("text-light bg-dark");
                 }
             });
@@ -67,65 +69,68 @@ while ($row = mysqli_fetch_assoc($result)) {
 </script>
 
 <body>
-    <div class="min-vh-100">
-        <main class="container">
-            <div class="row py-4">
-                <div class="col">
-                    <h1>Dashboard</h1>
-                </div>
-                <div class="col">
-                    <div class="d-flex justify-content-end">
-                        <a href="./create_project.php" class="btn btn-primary">Create New Project</a>
-                    </div>
-                </div>
-            </div>
+    <main class="container">
+        <div class="row py-4">
+            <h1>Dashboard</h1>
+        </div>
 
-            <div class="row flex-column flex-md-row">
-                <?php foreach ($array as $project) { ?>
-                    <div class="col col-md-3 mb-3 mb-sm-0">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">
-                                    <?php echo $project["project_title"]; ?>
-                                </h4>
-                                <!-- Convert date into number of days and display as subtitle -->
-                                <h6 class="card-subtitle mb-2 text-secondary no-dark">
-                                    <?php $days = (strtotime($project["due_date"]) - time()) / (60 * 60 * 24);
-                                    if ($days < 0) {
-                                        echo "Overdue by " . abs($days) . " days";
-                                    } else if ($days == 0) {
-                                        echo "Due today";
-                                    } else if ($days == 1) {
-                                        echo "Due tomorrow";
-                                    } else if ($days >= 365) {
-                                        echo "Due in " . round($days/365) . " years";
-                                    } else if ($days >= 30) {
-                                        echo "Due in " . round($days/30) . " months";
-                                    } else if ($days >= 14) {
-                                        echo "Due in " . round($days/7) . " weeks";
-                                    } else {
-                                        echo "Due in " . $days . " days";
-                                    }
-                                    echo " on " . $project["due_date"]; ?>
-                                </h6>
-                                <?php if ($project["total_tasks"] > 0) {
-                                    $completionPercentage = $project["completed_tasks"] / $project["total_tasks"] * 100;
+        <div class="row flex-column flex-md-row">
+            <?php foreach ($array as $project) { ?>
+                <div class="col col-md-3 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <?php echo $project["project_title"]; ?>
+                            </h4>
+                            <!-- Convert date into number of days and display as subtitle -->
+                            <h6 class="card-subtitle mb-2 text-secondary no-dark">
+                                <?php $days = (strtotime($project["due_date"]) - time()) / (60 * 60 * 24);
+                                if (ceil($days) == -1) {
+                                    echo "Overdue by 1 day";
+                                } else if (ceil($days) < 0) {
+                                    echo "Overdue by " . abs(ceil($days)) . " days";
+                                } else if (ceil($days) == 0) {
+                                    echo "Due today";
+                                } else if (ceil($days) == 1) {
+                                    echo "Due tomorrow";
+                                } else if ($days >= 365) {
+                                    echo "Due in " . ceil($days / 365) . " years";
+                                } else if ($days >= 30) {
+                                    echo "Due in " . ceil($days / 30) . " months";
+                                } else if ($days >= 14) {
+                                    echo "Due in " . ceil($days / 7) . " weeks";
                                 } else {
-                                    $completionPercentage = 0;
+                                    echo "Due in " . ceil($days) . " days";
                                 }
-                                ?>
-                                <div class="progress border no-dark" role="progressbar" aria-label="Completion Percentage" aria-valuenow="<?php echo $completionPercentage; ?>" aria-valuemin="0" aria-valuemax="100">
-                                    <div class="progress-bar no-dark" style="width: <?php echo $completionPercentage; ?>%"></div>
-                                </div>
-                                <p class="card-text">
-                                    <?php echo $project["description"]; ?>
-                                </p>
-                                <a href="view_team.php?project_ID=<?php echo $project["project_ID"]; ?>" class="btn btn-primary stretched-link">View</a>
+                                echo " on " . $project["due_date"]; ?>
+                            </h6>
+                            <?php if ($project["total_tasks"] > 0) {
+                                $completionPercentage = $project["completed_tasks"] / $project["total_tasks"] * 100;
+                            } else {
+                                // Avoid division by zero
+                                $completionPercentage = 0;
+                            }
+                            ?>
+                            <div class="progress border no-dark" role="progressbar" aria-label="Completion Percentage" aria-valuenow="<?php echo $completionPercentage; ?>" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar no-dark" style="width: <?php echo $completionPercentage; ?>%"></div>
                             </div>
+                            <p class="card-text">
+                                <?php if ($project["description"] == "") {
+                                    echo "No description available.";
+                                } else if (strlen($project["description"]) > 32) {
+                                    // If description is too long, add '...'
+                                    echo $project["description"] . "...";
+                                } else {
+                                    echo $project["description"];
+                                } ?>
+                            </p>
+                            <!-- Link to individual project page -->
+                            <a href="view_team.php?project_ID=<?php echo $project["project_ID"]; ?>" class="btn btn-primary stretched-link">View</a>
                         </div>
                     </div>
-                <?php } ?>
-            </div>
-        </main>
-    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </main>
+	<?php include "./footer.php"; ?>
 </body>
