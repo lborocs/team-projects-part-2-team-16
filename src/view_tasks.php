d13713dad81599e0f9e5ab8aff1ffe9573acf045
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-02-18 10:09:49 +0000

    for the a TL to view their own tasks

---
+++ src/view_tasks.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+	if (!isset($_SESSION["role"])) {
+		header('location: ./login.php');
+		//die();
+	} else if ($_SESSION["role"] == "Manager") {
+		$taskcreate = "link-dark";
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_m.php";
+		include "./dashboard_m.php";
+	} else if ($_SESSION["role"] == "TL") {
+		$topicview = "link-dark";
+		$taskcreate = "link-dark";
+		$taskview = "border-bottom border-primary link-primary";
+		$dashview = "link-dark";
+		include "./navbar_tl.php";
+		include "./dashboard_e.php";
+	} else if ($_SESSION["role"] == "Employee") {
+		$topicview = "link-dark";
+		$dashview = "border-bottom border-primary link-primary";
+		include "./navbar_e.php";
+		include "./dashboard_e.php";
+	}
+?>
